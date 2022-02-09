7c24a71e4839533c552e292303be57231e182c4c
OfficialAshish <officialashish270@gmail.com>
2022-02-09 23:59:09 +0530

    array update

--- Array_String/learning/testing.c
+++ Array_String/learning/testing.c
@@ -2,3 +2,13 @@
 #include <stdlib.h>
 
 int main(void)
+{
+    char *s={"hello"};
+    char str[]={"hello"};
+
+    //*s="s";
+    printf("%s\n",&s[0]);
+    //str[2]="";
+    printf("%s\n",str);
+}
+
