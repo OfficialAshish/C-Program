0c017ed18305fc56fce038386bc551341c2d0d15
Ashish Prajapati <officialashish270@gmail.com>
2022-07-16 23:47:06 -0400

    update

--- Project Type/Shop management /shop_management.c
+++ Project Type/Shop management /shop_management.c
@@ -1,46 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-
-#define COMP_SER_S 2
+#define COMP_SER_S (2+1)
 //#define MAX_SERIALNUM (26*26)
-#define PROD_SER_S (COMP_SER_S + 4)
+#define PROD_SER_S (COMP_SER_S + 4 +1)
 //(AB)DP84, 67600 diffrnt products for each company
-//#define SERIALNUM_SIZE 6
-//AA(E899), prod_series len>26k 
+#define TOT_PROD (676) //676*6bytes
 
-//structures 
+#define TOT_COMP_SER (676) //676*2bytes
 
-typedef struct date
-{
-    short unsigned int dd;
-    short unsigned int mm;
-    short unsigned int yy;
-} date;
-
-typedef struct productInfo
-{
-    char productName[30];
-    company productCompany;//its caste & series it belongs
-    char productSeries[PROD_SER_S];//6bytes total(AA)(BD89)
-    unsigned int productPrice;
-    date manufactureDate;
-    date expiryDate;
-    unsigned int productQuantity;
-    short int  productRating =4 ; //default
-} productInfo;
-
-/* //not required ,this way
-typedef struct serialNumber
-{
-    //char series[MAX_SERIALNUM][SERIALNUM_SIZE];    
-}s_serialNum;
-*/
+// structures
 
 typedef struct company
 {
     char companyName;
-    char companySeries[COMP_SER_S];//ex. (AA)2bytes
-    short int companyRating =4;
-    
-    //serialNumber companySeries;
-    
-}s_company;
+    // char companySeries[TOT_COMP_SER][COMP_SER_S]; // ex. (AA)2bytes
+    char companySeries[COMP_SER_S]; // ex. (AA)2bytes
+    short int companyRating;
+
+} s_company;
+
+typedef struct productInfo
+{
+    char productName[30];
+    s_company productCompany;       // its caste & series it belongs
+    char productSeries[TOT_PROD][PROD_SER_S]; // 6bytes total(AA)(BD89)
+    unsigned int productPrice;
+    s_date manufactureDate;
+    s_date expiryDate;
+    unsigned int productQuantity;
+    short int productRating;
+} s_productInfo;
+
+typedef struct date
+{
+    unsigned short int dd;
+    unsigned short int mm;
+    unsigned short int yy;
+} s_date;
+
+
+
+/* //not required ,this way
+typedef struct serialNumber
+{
+    //char series[MAX_SERIALNUM][SERIALNUM_SIZE];
+}s_serialNum;
+*/
+
+//Global Declaration..
+s_productInfo totalProducts[TOT_PROD];
+
+
+// FUNCTIONS...
+int serialNumber(int);
+
+int main()
+{
+    //default example products..
+
+    strcpy(totalProducts[0].productName , "LG Laptop");
+    strcpy(totalProducts[0].productCompany.companyName , "Pvt. LG Limited" );
+    strcpy(totalProducts[0].productCompany.companySeries , "AA");
+    totalProducts[0].productCompany.companyRating = 7;
+    totalProducts[0].productPrice = 34000;
+    totalProducts[0].productQuantity = 29;
+    totalProducts[0].manufactureDate.dd = 2;
+    totalProducts[0].manufactureDate.mm = 4;
+    totalProducts[0].manufactureDate.yy = 2022;
+
+}
