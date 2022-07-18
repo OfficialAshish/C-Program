7bb090941a2ebea889822607e40cab266c64a7a0
Ashish Prajapati <officialashish270@gmail.com>
2022-07-18 13:57:17 -0400

    update

--- testing/t3.c
+++ testing/t3.c
@@ -1,8 +1,8 @@
-/* #include <stdio.h>
+#include <stdio.h>
 
-void func1();
-// int func2(int);
-static int  a[10];
+// void func1();
+// // int func2(int);
+// static int  a[10];
 // static int a;
 // int a;
 
@@ -10,36 +10,29 @@ int main()
 {
 
     // printf("\nInside main1, a=%d ,", a);
-    for (int i = 0; a[i] < 100; i)
-    {
-        printf("%d ,\n",++a[i]);
-    }
+    // for (int i = 0; a[i] < 100; i)
+    // {
+    //     printf("%d ,\n",++a[i]);
+    // }
 
     // func1();
     // printf("\nInside main2, a=%d ,", a);
     // int c = func2(a);
     // func1();
     // func2(a);
-    // printf("\nInside main3, a=%d ,\n", a);
+    printf("\nInside main3, a=%d ,\n", 'N');
     // printf("\nInside main3, c=%d ,\n", c);
     return 0;
 }
 
-void func1()
-{
-    int a;
-    // static int a;
-    a++;
-    a++;
-    printf("\nInside func1, a=%d ,", a);
-    printf("\nInside func1, b=%d ,\n", a);
-}
- */
+// void func1()
+// {
+//     int a;
+//     // static int a;
+//     a++;
+//     a++;
+//     printf("\nInside func1, a=%d ,", a);
+//     printf("\nInside func1, b=%d ,\n", a);
+// }
+ 
 
-#include <stdio.h>
-
-
-int main()
-{
-    printf("%d" , 'Z');
-}
